615ae40ecdc285e4583a8865070350e880587308
Christian Sperandio <christian.sperandio@gmail.com>
2016-04-22 21:41:44 +0200

    Comments.

--- src/main/kotlin/domain/processing/entities/Step.kt
+++ src/main/kotlin/domain/processing/entities/Step.kt
@@ -5,6 +5,13 @@ import domain.processing.entities.objects.RunningState
 /**
  * Created by Christian Sperandio on 20/04/2016.
  *
+ * Interface for the (sub)steps of tasks.
+ *
+ * @property state Current state of the step.
+ * @property steps Substeps.
+ * @property parameters Parameters of the step.
+ * @property name Step name.
+ *
  */
 interface Step {
     var state: RunningState?
@@ -12,10 +19,27 @@ interface Step {
     val parameters: Set<Parameter>?
     val name: String?
 
+    /**
+     * Sets to null the ID of the current step.
+     */
     fun resetId()
 
+    /**
+     * Returns the substep with the given name.
+     * **Note:** Each substep should have an unique name.
+     *
+     * @param name
+     * @return The substep or null if no step has this name.
+     */
     fun stepByName(name: String): Step? = steps?.first { it.name == name }
 
+    /**
+     * Returns the parameter with the given name.
+     * **Note:** Each parameter for a step should have an unique name.
+     *
+     * @param name
+     * @return The parameter or null if none has this name.
+     */
     fun parameterByName(name: String): Parameter? = parameters?.first { it.name == name }
 
 }
--- src/main/kotlin/domain/processing/entities/Task.kt
+++ src/main/kotlin/domain/processing/entities/Task.kt
@@ -5,21 +5,53 @@ import domain.processing.entities.objects.RunningState
 /**
  * Created by Christian Sperandio on 20/04/2016.
  *
+ * Interface for the tasks.
+ *
+ * @property state The current task state.
+ * @property steps The steps of the task.
+ * @property parameters The root task parameters used for step parameters initalization.
+ * @property name Task name
+ *
  */
 interface Task {
-    fun resetId()
-
     var state: RunningState?
     val steps: Set<Step>?
     val parameters: Set<Parameter>?
     var name: String?
 
+    /**
+     * Set to null the ID of the task.
+     */
+    fun resetId()
+
+    /**
+     * Changes the value of a task parameter. When a task parameter is changed, all linked step parameters are
+     * updated too.
+     *
+     * @param parameterName Name of the parameter to update.
+     * @param value The new value of the parameter.
+     */
     fun updateParameter(parameterName: String, value: String) {
         parameters?.filter { it.name == parameterName }?.forEach { it.value = value }
     }
 
+    /**
+     * Returns the step of the task given its name.
+     * **Note:** The steps of the same task shouldn't share the same name.
+     *
+     * @param name The step name.
+     * @return The found step or null.
+     */
     fun stepByName(name: String): Step? = steps?.first { it.name == name }
 
+
+    /**
+     * Returns the parameter of the task given its name.
+     * **Note:** The parameters of the same task shouldn't share the same name.
+     *
+     * @param name The parameter name.
+     * @return The found parameter or null.
+     */
     fun parameterByName(name: String): Parameter? = parameters?.first { it.name == name }
 
 }
