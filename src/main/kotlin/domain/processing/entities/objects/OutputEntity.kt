5b6a036f0016652d9ddbb021da07b17d44c73d46
Christian Sperandio <christian.sperandio@gmail.com>
2016-05-03 21:13:23 +0200

    Bug fix.

--- src/main/kotlin/domain/processing/entities/objects/OutputEntity.kt
+++ src/main/kotlin/domain/processing/entities/objects/OutputEntity.kt
@@ -4,11 +4,13 @@ import domain.global.entities.NamedEntity
 import domain.global.validators.EntityValidator
 import domain.global.validators.OutputValidator
 import domain.global.validators.Validable
+import org.neo4j.ogm.annotation.NodeEntity
 
 /**
  * Created by Christian Sperandio on 03/05/2016.
  *
  */
+@NodeEntity(label = "Output")
 class OutputEntity(_name: String = "") : NamedEntity(_name), Validable<OutputEntity> {
     var value: String? = null
 
